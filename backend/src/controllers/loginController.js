14cb800a3e942671dbba5ecce31a3dd47a2874cb
Guillermoabril <guillermo.abrilr@gmail.com>
2023-10-04 00:41:43 -0500

    update 2 angular

---
+++ backend/src/controllers/loginController.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcryptjs");
+const UserModel = require("../models/usersModels");
+const getToken = require("../helpers/gets");
+
+const loginController = {
+  loginUser: async (request, response) => {
+    try {
+      const { email, password } = request.body;
+      const userFound = await UserModel.findOne({ email });
+      if (userFound) {
+        const isValidPassword = await bcrypt.compare(
+          password,
+          userFound.password
+        );
+        if (isValidPassword) {
+          const token = await getToken({
+            id: userFound._id,
+            name: userFound.name,
+          });
+          response.json(token);
+        } else {
+          response.json({ message: "Invalid Credentials" });
+        }
+      } else {
+        response.json({ message: "Invalid Credentials" });
+      }
+    } catch (error) {
+      response.json({ message: "Fail during login user" });
+    }
+  },
+};
+
+module.exports = loginController;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,21 @@ aqui se guarda toda la configuracion del sevidor
 
 const express = require("express"); // estamos importando express
 const morgan = require("morgan"); // morgan me ayuda a visualizar logs o peticiones al servidor
+const cors = require("cors");
 const usersRouter = require("./routers/usersRouter"); // se importan las rutas
 const clientsRouter = require("./routers/clientsRouter");
+const loginRouter = require("./routers/loginRouter");
 
 const server = express(); // estamos creando un servidor
 const port = process.env.PORT; //aqui se especifica por que puerto se requiere que corra el servidor o aplicacion
 
 server.set("port", port); //
 
+server.use(cors());
 server.use(express.json());
-
 server.use(morgan("dev")); // aqui indico que el servidor va a ausar morgan
 
+server.use("/login/", loginRouter);
 server.use("/users/", usersRouter); // le indicamos el nombre de la ruta que queremo usar para consultar las rutas importadas desde el postman
 server.use("/clients/", clientsRouter);
 
